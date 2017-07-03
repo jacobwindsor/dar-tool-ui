48bfbe46dd78769c2596bcc6f0bbd2accce022a1
Jacob Windsor <jacob.windsor@ymail.com>
2017-07-03 17:36:58 +0200

    Style error message

--- src/components/ErrorMessage/index.jsx
+++ src/components/ErrorMessage/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import Paper from 'material-ui/Paper';
 import PropTypes from 'prop-types';
+import alert from '../../assets/alert.svg';
+import './index.css';
 
 const ErrorMessage = ({ errorMessage }) => (
-  <Paper>
+  <Paper className="error-message">
+    <img src={alert} alt="Alert" />
     <h1>Oops!</h1>
     <h4>{errorMessage}</h4>
   </Paper>
